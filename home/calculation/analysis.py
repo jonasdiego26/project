fba86e8be346edcfdb6a901d89a587c883b2b8e3
jonasdiego26 <jonasvandoorn26@gmail.com>
2022-05-09 21:34:58 +0200

    fix for sample size and single ztest

--- home/calculation/analysis.py
+++ home/calculation/analysis.py
@@ -12,8 +12,12 @@ def analyze(prob_1, prob_2):
     var1 = ab_test[ab_test['variant'] == 0].values[:,1]
     var2 = ab_test[ab_test['variant'] == 1].values[:,1]
 
+    sample_size1 = var1.sum()
+    sample_size2 = var2.sum()
+    total_sample_size = sample_size1 + sample_size2
+
     ztest_var1, propability_value_var1 = stests.ztest(x1=var1, x2=var2)
-    sample_size = var1.sum()
+    sample_size = total_sample_size
     alpha = 0.05
     power = 0.3
     power_analysis = TTestIndPower()
@@ -23,18 +27,13 @@ def analyze(prob_1, prob_2):
 
     effect_size1 = 'Maximum detectable effect size: {0:.2f}'.format(optimize.root_scalar(f, bracket=[0.01, 1.0]).root)
 
-
-    sample_size = var2.sum()
+    # not using this one (only one variant)
+    sample_size = sample_size2
     alpha = 0.05
     power = 0.3
     power_analysis = TTestIndPower()
 
     ztest_var2, propability_value_var2 = stests.ztest(var2)
-
-
-
     effect_size2 = 'Maximum detectable effect size: {0:.2f}'.format(optimize.root_scalar(f, bracket=[0.01, 1.0]).root)
-    sample_size1 = var1.sum()
-    sample_size2 = var2.sum()
-
-    return prob_1, ztest_var1, propability_value_var1, effect_size1, prob_2, ztest_var2, propability_value_var2, effect_size2, sample_size1, sample_size2, ab_test
+    
+    return prob_1, ztest_var1, propability_value_var1, effect_size1, prob_2, ztest_var2, propability_value_var2, effect_size2, sample_size1, sample_size2, ab_test, total_sample_size, power
--- home/views.py
+++ home/views.py
@@ -12,7 +12,10 @@ def Home(request):
     ab_test_var = analyze(30, 50)
     posts = post.objects.all()
     df = ab_test_var[10]
-    context= {'df':df,'posts': posts, 'chance_1':ab_test_var[0], 'z_value_1':ab_test_var[1], 'p_value_1': ab_test_var[2], 'effect_size_1':ab_test_var[3], 'chance_2':ab_test_var[4], 'z_value_2':ab_test_var[5], 'p_value_2': ab_test_var[6], 'effect_size_2':ab_test_var[7], 'sample_size1': ab_test_var[8], 'sample_size2':ab_test_var[9]}
+    context= {'total_sample_size':ab_test_var[11],'df':df,'posts': posts, 'chance_1':ab_test_var[0], 
+    'z_value_1':ab_test_var[1], 'p_value_1': ab_test_var[2], 'effect_size_1':ab_test_var[3], 'chance_2':ab_test_var[4], 
+    'z_value_2':ab_test_var[5], 'p_value_2': ab_test_var[6], 'effect_size_2':ab_test_var[7], 'sample_size1': ab_test_var[8], 
+    'sample_size2':ab_test_var[9], 'power':ab_test_var[12]}
     if request.GET:
         response = HttpResponse('')
         response['Content-Disposition'] = 'attachment; filename=file.csv'   
